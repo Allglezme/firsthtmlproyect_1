bad8b3115406e87cfa4f8330994e420b91c38fd5
Allglezme <116189382+Allglezme@users.noreply.github.com>
2022-11-02 10:41:04 -0600

    se agrego unirse

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express =require("express")
 
 const app = express()
 
-app.get("/", (req,res) => {
-    res.send("Hola") 
+const Jugadores = []
+
+class Player {
+    constructor(id){
+        this.id = id
+        
+    }
+}
+
+app.get("/unirse", (req,res) => {
+   const id = `${Math.random()}`
+
+   const player = new Player (id)
+
+   Jugadores.push(player)
+    res.send(id) 
 })
 
 app.listen(8080, () => {
