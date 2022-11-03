30649146b5f87ba262d40587bd6ea7d4b341e87b
ALIS <alisglezmon@gmail.com>
2022-11-02 18:24:12 -0600

    seinstalounservidor

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express =require("express")
+const cors =require("cors")
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
 
 const Jugadores = []
 
@@ -9,15 +13,39 @@ class Player {
         this.id = id
         
     }
+    asignarMokepon(mokepon){
+        this.mokepon  = mokepon
+    }
 }
-
+class Mokepon{
+    constructor(name){
+        this.name  = name
+    }
+}
 app.get("/unirse", (req,res) => {
    const id = `${Math.random()}`
 
    const player = new Player (id)
 
    Jugadores.push(player)
+    res.setHeader('Access-Control-Allow-Origin',"*")
     res.send(id) 
+})
+
+
+app.post("/mokepon/:jugadorId",(req,res) => {
+    const jugadorId = req.params.jugadorId || ""
+    const name = req.body.mokepon|| ""
+    const mokepon = new Mokepon(name)
+    console.log(Jugadores)
+    const jugadorIndex  = Jugadores.findIndex((jugador) =>jugadorId ===jugador.id)
+
+    if(jugadorIndex >= 0){
+        Jugadores[jugadorIndex].asignarMokepon(mokepon)
+    }
+
+    console.log(jugadorId)
+    res.end()
 })
 
 app.listen(8080, () => {
--- js/pagepry.js
+++ js/pagepry.js
@@ -21,6 +21,8 @@ const contenedorAtaques = document.getElementById("contenedorAtaques")
 const sectionVerMapa = document.getElementById("verMapa")
 const mapa = document.getElementById("mapa")
 
+let jugadorId = null
+
 let mokepones = []
 
 let ataqueJugador = []
@@ -229,6 +231,19 @@ function iniciarJuego() {
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
    
     botonReiniciar.addEventListener("click",reiniciarJuego)
+    unirseAlJuego()
+}
+function unirseAlJuego(){
+    fetch("http://localhost:8080/unirse")
+    .then(function(res) {
+        if (res.ok){
+            res.text()
+                .then(function(respuesta){
+                    console.log(respuesta)
+                    jugadorId = respuesta
+                })   
+        }
+    })
 }
 //Pantalla inicial
 function seleccionarMascotaJugador() {
@@ -266,8 +281,22 @@ function seleccionarMascotaJugador() {
 
         sectionVerMapa.style.display = "flex"
         iniciarMapa() 
+
+        seleccionarMokepon(mascotaJugador)
         
     }
+    
+}
+function seleccionarMokepon(mascotaJugador){
+    fetch(`http://localhost:8080/mokepon/${jugadorId}`,{
+        method:"post",
+        headers : {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            mokepon: mascotaJugador
+        })
+    })
 }
 function extraerAtaques(mascotaJugador){
     let ataques
